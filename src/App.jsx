172603f879289257ccf93d70ab7da0094d8be211
washington maina <103877612+washington254@users.noreply.github.com>
2025-03-23 15:39:40 +0300

    fight mk

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,23 @@ import { UI } from "./components/UI";
 function App() {
   return (
     <>
-    <UI/>
+    {/* <UI/> */}
     <Loader  />
 
     <Canvas 
     shadows 
-    camera={{ position: [0, 0, 13], fov: 40 }}
+    camera={{ position: [0, 0, 9], fov: 40 }}
     onCreated={({ gl }) => {
       gl.toneMappingExposure = 0.05;
 
     }}
     >
-      <color attach="background" args={["#232323"]} />
       <Suspense fallback={null}>
         <Experience />
       </Suspense>
         <EffectComposer>
-            <Bloom luminanceThreshold={0} luminanceSmoothing={0.7} height={300} />
-            <Noise opacity={0.02} />
+            {/* <Bloom luminanceThreshold={0} luminanceSmoothing={0.7} height={300} />
+            <Noise opacity={0.02} /> */}
           </EffectComposer>
     </Canvas>
     </>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,25 +1,28 @@
-import { Environment, Loader, OrbitControls, PivotControls } from "@react-three/drei";
-import { Hoodie } from "./Hoodie";
-import { Showroom } from "./Showroom";
-import { Hoodie2 } from "./Hoodie2";
+import { Environment, Loader, OrbitControls, PivotControls, Sky } from "@react-three/drei";
+import { Fight } from "./Fight";
+
 
 export const Experience = () => {
   return (
     <>
   <OrbitControls
-  maxPolarAngle={Math.PI / 2}     // Limit up/down
-  minPolarAngle={Math.PI / 2}     // Lock vertical rotation to horizontal view
-  minDistance={2}                 // Minimum zoom distance
-  maxDistance={18}                // Maximum zoom distance
-  minAzimuthAngle={-Math.PI / 5}      // Limit left rotation (-180 degrees)
-  maxAzimuthAngle={Math.PI / 5}       // Limit right rotation (180 degrees)
+  // maxPolarAngle={Math.PI / 2}     // Limit up/down
+  // minPolarAngle={Math.PI / 2}     // Lock vertical rotation to horizontal view
+  // minDistance={2}                 // Minimum zoom distance
+  // maxDistance={18}                // Maximum zoom distance
+  // minAzimuthAngle={-Math.PI / 5}      // Limit left rotation (-180 degrees)
+  // maxAzimuthAngle={Math.PI / 5}       // Limit right rotation (180 degrees)
 />
 
-      <ambientLight intensity={1} />
-      <Showroom position-y={-3.5} position-z={-4} scale={1.5} />
-
-      <Hoodie2 position-y={-2.5} scale={0.7} />
-      <Environment preset="city" environmentIntensity={1.2} />
+        <ambientLight intensity={1} />
+       <Fight scale={4.8} position={[0, -1.2, 0]}  rotation={[0.2, 0, 0]}  />
+       <Sky
+             distance={450000}
+             sunPosition={[5, 1, 8]}
+             inclination={0}
+             azimuth={0.25}
+         />
+      <Environment preset="city" environmentIntensity={1.6} />
     </>
   );
 };
